eacc3a6b18591b239dcebae4e91cf0163849e26f
Mike Wilcox <mwilcox56@gmail.com>
2016-03-25 02:32:13 -0400

    add search fn

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'promise-polyfill';
+
+export default function search(term) {
+  return new Promise((resolve, reject) => {
+    var options = {
+      host: 'airports.p.mashape.com',
+      port: 443,
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-Mashape-Key': 'NyE91agj31mshGnoZv7CfnjEQbNmp1woGA1jsnYy2rssyyUz6y'
+      }
+    };
+
+    var req = https.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', resolve);
+    });
+
+    req.on('error', reject);
+
+    req.write(JSON.stringify({ search: term }));
+    req.end();
+  });
+}
